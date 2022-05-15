771ca766eab427dd346ccede49892deb69ca3d11
yutohub <83153839+yutohub@users.noreply.github.com>
2022-05-15 18:13:43 +0900

    Added solution for 0141

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # leetcode
-Solutions to LeetCode
+
+I would like to collect my own solutions to LeetCode.
+
+## Solution
+
+The languages I plan to use are `Python3` and `Go`
+
+## Plan
+
+I am collecting the solutions for my own study and do not plan to cover all of them.
+
+If you have any advice or questions, please feel free to contact us.
---
+++ leetcode/0141_Linked_List_Cycle/0141_Linked_List_Cycle.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head):
+        try:
+            fast = head.next.next
+            slow = head.next
+            while fast != slow:
+                fast = fast.next.next
+                slow = slow.next
+            return True
+        except:
+            return False
